07e7e1a792c07bd83b96044d4cbf4df9951dccc6
David Marchante <cdmarchante@gmail.com>
2019-07-05 12:00:55 -0700

    add basic funtionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './app.scss';
+
+import mockData from './mock.json';
+
+const API = 'http://cdmarch-taskmaster-dev2.us-west-2.elasticbeanstalk.com/tasks'
+
+function Tasks() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const _getTasks = () => {
+    // setTasks(mockData)
+    // fetch from brook
+    fetch( API, {
+      mode:'cors',
+    })
+    .then( data => data.json() )
+    .then( task => setTasks(task) )
+    .catch( console.error );
+  };
+
+  const _toggleStatus = (e) => {
+    e.preventDefault();
+    let id = e.target.id;
+    // setTasks(tasks.map( (task) =>
+    //   task.id !== id ? task : {...task, status:!task.status}
+    // ));
+
+    // patch to Brooks api
+    fetch( `${API}/${id}/status`, {
+      mode:'cors',
+      method: 'PATCH'
+    })
+    .then(data => data.json())
+    .then(task => {
+      setTasks(tasks.map( (entry) => {
+          return entry.id === id ? task : entry;
+        }
+      ));
+    })
+    .catch( console.error );
+  };
+
+  useEffect(_getTasks, []);
+
+  return (
+    <ul>
+      {tasks.map( (task) =>
+        <li className={`status-${task.status}`} key={task.id}>
+          {/* <details> */}
+            {/* <summary> */}
+              <span>{task.title}</span>
+              {/* <span id={task.id}>{task.status.toString()}</span> */}
+              <span id={task.id}>{task.status}</span>
+            {/* </summary> */}
+            {/* <History history={person.history} /> */}
+          {/* </details> */}
+        </li>
+      )}
+    </ul>
+  )
+}
+
+function History(props) {
+  let history = props.history || [];
+  return (
+    <section>
+      {history.map( (item,idx) =>
+        <div>
+          <span>{item.itemName}</span>
+          <span>{item.price}</span>
+        </div>
+      )}
+    </section>
+  )
+}
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>Taskmaster</header>
+      <main>
+        <Tasks />
+      </main>
+      <footer>&copy; 2019 Taskmaster</footer>
+      </>
   );
 }
 
